05d87011590996970a1287498bdc0af6584321f6
billyw03 <yohanesbillywicaksono_2327240155@mhs.mdp.ac.id>
2026-05-26 10:27:12 +0700

    menambahkan post services

---
+++ lookcut_app/lib/services/post_services.dart
@@ -0,0 +1,99 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:pangkas_mbut/models/post_model.dart';
+
+class PostService {
+  // COLLECTION
+  static CollectionReference postCollection = FirebaseFirestore.instance
+      .collection('posts');
+
+  // ADD POST
+  static Future<void> addPost(PostModel post) async {
+    try {
+      await postCollection.add(post.toDocument());
+    } catch (e) {
+      debugPrint('Add Post Error : $e');
+    }
+  }
+
+  // GET ALL POSTS
+  static Stream<List<PostModel>> getPostList() {
+    return postCollection
+        .orderBy('created_at', descending: true)
+        .snapshots()
+        .map(
+          (snapshot) =>
+              snapshot.docs.map((doc) => PostModel.fromDocument(doc)).toList(),
+        );
+  }
+
+  // GET POST BY CATEGORY
+  static Stream<List<PostModel>> getPostByCategory(String category) {
+    return postCollection
+        .where('category', isEqualTo: category)
+        .snapshots()
+        .map((snapshot) {
+          final posts = snapshot.docs
+              .map((doc) => PostModel.fromDocument(doc))
+              .toList();
+
+          posts.sort((a, b) {
+            final aDate = a.createdAt?.toDate();
+            final bDate = b.createdAt?.toDate();
+
+            if (aDate == null && bDate == null) {
+              return 0;
+            }
+
+            if (aDate == null) {
+              return 1;
+            }
+
+            if (bDate == null) {
+              return -1;
+            }
+
+            return bDate.compareTo(aDate);
+          });
+
+          return posts;
+        });
+  }
+
+  // GET USER POSTS
+  static Stream<List<PostModel>> getUserPosts(String userId) {
+    return postCollection
+        .where('user_id', isEqualTo: userId)
+        .orderBy('created_at', descending: true)
+        .snapshots()
+        .map(
+          (snapshot) =>
+              snapshot.docs.map((doc) => PostModel.fromDocument(doc)).toList(),
+        );
+  }
+
+  // GET SINGLE POST
+  static Future<PostModel?> getSinglePost(String postId) async {
+    try {
+      final DocumentSnapshot document = await postCollection.doc(postId).get();
+
+      return PostModel.fromDocument(document);
+    } catch (e) {
+      debugPrint('Get Single Post Error : $e');
+
+      return null;
+    }
+  }
+
+  static Future<void> deletePost(PostModel post) async {
+    if (post.id == null) {
+      return;
+    }
+
+    try {
+      await postCollection.doc(post.id).delete();
+    } catch (e) {
+      debugPrint('Delete Post Error : $e');
+    }
+  }
+}
