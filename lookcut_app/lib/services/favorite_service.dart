725ad3a34b1199947d66c872299c8b9c4b2b0afc
Nasrudin <muhammadnasrudin674@gmail.com>
2026-05-28 19:31:16 +0700

    update Favorite screen

--- lookcut_app/lib/services/favorite_service.dart
+++ lookcut_app/lib/services/favorite_service.dart
@@ -50,3 +50,64 @@ class FavoriteService {
       );
     }
   }
+  // GET FAVORITES
+  static Stream<List<PostModel>>
+      getFavorites(
+    String userId,
+  ) {
+
+    return favoriteCollection
+        .where(
+          'user_id',
+          isEqualTo: userId,
+        )
+        .snapshots()
+        .map(
+          (snapshot) {
+            final posts = snapshot.docs
+                .map(
+                  (doc) {
+
+                    final data =
+                        doc.data()
+                            as Map<String, dynamic>;
+
+                    return PostModel(
+                      id: data['post_id'],
+                      image: data['image'],
+                      barberName:
+                          data['barber_name'],
+                      description:
+                          data['description'],
+                      category: data['category'],
+                      latitude: data['latitude'],
+                      longitude:
+                          data['longitude'],
+                      userId: data['post_user_id'],
+                      userFullName:
+                          data['user_full_name'],
+                      createdAt: data['created_at'],
+                    );
+                  },
+                )
+                .toList();
+
+            posts.sort((a, b) {
+              final aDate = a.createdAt?.toDate();
+              final bDate = b.createdAt?.toDate();
+              if (aDate == null && bDate == null) {
+                return 0;
+              }
+              if (aDate == null) {
+                return 1;
+              }
+              if (bDate == null) {
+                return -1;
+              }
+              return bDate.compareTo(aDate);
+            });
+
+            return posts;
+          },
+        );
+  }
